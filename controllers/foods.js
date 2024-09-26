6a6e37e64cc767c11c48acf357f91dfd2c75c490
MartyV33 <martinverdoza33@gmail.com>
2024-09-26 13:30:22 -0500

    created the index.ejs and new.ejs files under the foods folder

--- controllers/foods.js
+++ controllers/foods.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+router.get('/users/:userId/foods/new', (req, res) =>{
+    res.render('foods/new.ejs', { userId: req.params.userId });
+});
+
 router.get('/', (req, res) => {
     res.render('foods/index.ejs');
 })
